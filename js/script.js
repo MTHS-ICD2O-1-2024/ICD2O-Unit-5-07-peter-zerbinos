6898252b2afb8b18385237a4160e420f5c0c8a3b
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-16 01:32:19 +0000

    added some code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates the sum of numbers.
+ */
+function calculateSumOfNumbers() {
+  const userNumber = parseInt(document.getElementById("user-number").value)
+  let sumOfNumbers = 0
+
+  for (let counter = userNumber; counter > 0; counter--) {
+    sumOfNumbers += counter
+  }
+
+  document.getElementById("result").innerText =
+    "The sum of all the natural numbers up to " + userNumber + " is " + sumOfNumbers + "."
+}
